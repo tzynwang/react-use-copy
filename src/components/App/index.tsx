07888f1f4b37b904ae460bfc25067d7d9497a45a
Tzu Yin <tzyn.wang@gmail.com>
2022-05-22 14:56:29 +0800

    [feat] Add copy demo

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,31 +1,59 @@
-import React, { memo } from 'react'
-import classNames from 'classnames'
-
-import Styles from './index.module.css'
+import React, { memo, useRef } from 'react';
+import useCopy from '@Hooks/useCopy';
 
 function App(): React.ReactElement {
+  // States
+  const listRef = useRef<HTMLDivElement | null>(null);
+
+  // Functions
+  const handleCopyText = (): void => {
+    useCopy('copy pure text', () => console.info('success'));
+  };
+  const handleCopyLink = (): void => {
+    useCopy(listRef.current, () => console.info('success'));
+  };
+
+  // Main
   return (
-    <main className={classNames(Styles.main)}>
-      <div>
-        Hello world, this React APP is created by{' '}
-        <code className={classNames(Styles.mainCode)}>npx create-react-app with template --choffee</code>.
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col">
+        <div className="text-5xl text-center font-bold">copy testing</div>
+        <div className="flex gap-4">
+          <button onClick={handleCopyText} className="btn btn-secondary">
+            copy pure text
+          </button>
+          <button onClick={handleCopyLink} className="btn btn-primary">
+            copy list of anchors
+          </button>
+        </div>
+        <div ref={listRef}>
+          <ul className="list-disc">
+            <li>
+              <a
+                href="https://developer.mozilla.org/en-US/docs/Web/API/ClipboardItem/ClipboardItem"
+                className="link"
+              >
+                MDN: ClipboardItem()
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.joshwcomeau.com/css/surprising-truth-about-pixels-and-accessibility/"
+                className="link"
+              >
+                The Surprising Truth About Pixels and Accessibility
+              </a>
+            </li>
+            <li>
+              <a href="https://axios-http.com/docs/req_config" className="link">
+                axios Request Config
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div>
-        Author: Charlie (Tzu Yin) |{' '}
-        <a href="https://github.com/tzynwang" target="_blank" className={Styles.mainAnchor}>
-          GitHub
-        </a>{' '}
-        |{' '}
-        <a href="https://tzynwang.github.io/" target="_blank" className={Styles.mainAnchor}>
-          Blog
-        </a>{' '}
-        |{' '}
-        <a href="https://www.npmjs.com/~tzyn.wang" target="_blank" className={Styles.mainAnchor}>
-          npm Packages
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
 
-export default memo(App)
+export default memo(App);
